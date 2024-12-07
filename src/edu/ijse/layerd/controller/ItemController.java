f2acdc65eacf072288362a04ad21b1297875b195
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-12-07 11:58:58 +0530

    View, controller and sto for item

---
+++ src/edu/ijse/layerd/controller/ItemController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layerd.controller;
+
+import edu.ijse.layerd.dto.ItemDto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anjan
+ */
+public class ItemController {
+    public String save(ItemDto dto) throws Exception{
+        return null;
+    }
+    public String update(ItemDto dto) throws Exception{
+        return null;
+    }
+    public String delete(String itemCode) throws Exception{
+        return null;
+    }
+    public String search(String itemCode) throws Exception {
+        return null;
+    }
+    public ArrayList<ItemDto> getAll() throws Exception{
+        return null;
+    }
+}
