e3733581278da598bab95eae06f0350e212e2f55
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-12-07 11:31:18 +0530

    DBConnection

--- src/edu/ijse/layerd/Main.java
+++ src/edu/ijse/layerd/Main.java
@@ -4,6 +4,9 @@
  */
 package edu.ijse.layerd;
 
+import edu.ijse.layerd.db.DBConnection;
+import java.sql.SQLException;
+
 /**
  *
  * @author anjan
@@ -13,8 +16,9 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         System.out.println("Working");
+        DBConnection.getInstance().getConnection();
     }
     
 }
