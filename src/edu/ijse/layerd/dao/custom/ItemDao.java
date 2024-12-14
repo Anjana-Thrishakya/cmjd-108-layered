0261c1add9e3aca00c2f5cf97080ff22b4d139ea
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-12-14 09:17:33 +0530

    ItemDao and Impl

---
+++ src/edu/ijse/layerd/dao/custom/ItemDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.ijse.layerd.dao.custom;
+
+import edu.ijse.layerd.dao.CurdDao;
+import edu.ijse.layerd.entity.ItemEntity;
+
+/**
+ *
+ * @author anjan
+ */
+public interface ItemDao extends CurdDao<ItemEntity, String>{
+    
+}
---
+++ src/edu/ijse/layerd/dao/custom/impl/ItemDaoImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layerd.dao.custom.impl;
+
+import edu.ijse.layerd.dao.custom.ItemDao;
+import edu.ijse.layerd.entity.ItemEntity;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anjan
+ */
+public class ItemDaoImpl implements ItemDao{
+
+    @Override
+    public boolean save(ItemEntity t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(ItemEntity t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ItemEntity search(String id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<ItemEntity> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
